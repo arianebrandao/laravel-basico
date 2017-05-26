e673d6e1522a8f9e17310b63aa98f45688afdfa9
Ariane Brandão <ariane_mad@hotmail.com>
2017-05-26 14:33:03 -0300

    aula4
    Rotas

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,1 @@
+View Página empresa
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,50 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/empresa', function(){
+	return view('empresa');
+});
+
+Route::match(['get', 'post'], '/match', function(){
+	return 'Rota match';
+});
+
+Route::get('/tessste/teste/', function(){
+	return 'Rota grande!';
+})->name('rota.nomeada');
+
+Route::get('/redireciona', function(){
+	return redirect()->route('rota.nomeada');
+});
+
+
+Route::get('/categoria/{idCategoria}', function($idCategoria){
+	return "Posts da categoria {$idCategoria}";
+});
+
+Route::get('/categoria/{idCategoria}/nome-fixo/{prm2}', function($idCategoria, $prm2){
+	return "Posts da categoria {$idCategoria} - {$prm2}";
+});
+
+//valor opcional
+Route::get('/categoria2/{idCategoria?}', function($idCategoria=null){
+	return "Posts da categoria {$idCategoria}";
+});
+
+//grupo de rotas
+Route::group(['prefix' => 'painel', 'middleware' => 'auth'], function(){
+	Route::get('/usuarios', function(){
+		return "Usuários";
+	});
+	Route::get('/financeiro', function(){
+		return "Financeiro";
+	});
+	Route::get('/', function(){
+		return "Dashboard";
+	});
+});
+
+Route::get('/login', function(){
+	return "Formulário de login";
+});
